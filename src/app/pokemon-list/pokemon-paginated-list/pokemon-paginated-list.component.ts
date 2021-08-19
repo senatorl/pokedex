a7ff0d082281533fe6b1eb839265acbd09e273e9
Lukasz Senator <l.senator@hotmail.com>
2021-08-19 10:28:57 +0200

    cleaning due to removal of mat-grid

--- src/app/pokemon-list/pokemon-paginated-list/pokemon-paginated-list.component.ts
+++ src/app/pokemon-list/pokemon-paginated-list/pokemon-paginated-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PokemonService } from '../../resources/pokemon/pokemon.service';
 import { PokemonListStoreService } from '../../stores/pokemon-list/pokemon-list-store.service';
 import { PageEvent } from '@angular/material/paginator';
@@ -12,25 +12,13 @@ export class PokemonPaginatedListComponent implements OnInit {
   public pageSize = 12;
   public sliceStart = 0;
   public sliceEnd = this.pageSize;
-  public gridCols = 3;
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event: UIEvent) {
-    const target = event.target as Window;
-    this.setGridCols(target.screen.width);
-  }
 
   constructor(
     public pokemonService: PokemonService,
     public pokemonListStoreService: PokemonListStoreService
   ) {}
 
-  setGridCols(screenWidth: number) {
-    this.gridCols = screenWidth < 1000 ? 1 : 3;
-  }
-
   ngOnInit(): void {
-    this.setGridCols(window.screen.width);
     this.pokemonService.getAllFilteredByName();
   }
 
